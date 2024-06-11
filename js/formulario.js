8403a00324d3092cb32ea813a002028339c5ff79
Vinicius Andrade <Viniciusjandrade08@gmail.com>
2024-06-11 18:18:38 -0300

    finalizado for,

--- js/formulario.js
+++ js/formulario.js
@@ -6,19 +6,24 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const nome = document.getElementById("fname").value;
     const email = document.getElementById("lname").value;
-    const necessidade = document.getElementById('cars').value;
+    const necessidade = document.getElementById("cars").value;
     const comentario = document.getElementById("lt").value;
 
-    const validacao = (campo, classErro,Input, mensagemErro) => {
+    const validacao = (campo, classErro, Input, mensagemErro) => {
       if (campo === "") {
         document.querySelector(classErro).innerHTML = mensagemErro;
-        document.querySelector(Input).classList.add('inputError')
+        document.querySelector(Input).classList.add("inputError");
       }
     };
 
-    validacao(nome, ".error-name",".input-name","Digite seu nome");
-    validacao(email, ".error-email",".input-email","Digite seu email");
-    validacao(comentario,".error-comentario",".input-coment","Digite um comentário");
+    validacao(nome, ".error-name", ".input-name", "Digite seu nome");
+    validacao(email, ".error-email", ".input-email", "Digite seu email");
+    validacao(
+      comentario,
+      ".error-comentario",
+      ".input-coment",
+      "Digite um comentário"
+    );
 
     if (nome != "" && email != "" && comentario != "") {
       const meng = [];
@@ -29,12 +34,16 @@ window.addEventListener("DOMContentLoaded", () => {
         comentario: comentario,
       };
 
+      
       meng.push(toSend);
       console.log(meng);
-
-
+      limparCampo();
     }
   });
 });
 
-
+function limparCampo() {
+  document.getElementById("fname").value = ""
+  document.getElementById("lname").value = ""
+  document.getElementById("lt").value = ""
+}
